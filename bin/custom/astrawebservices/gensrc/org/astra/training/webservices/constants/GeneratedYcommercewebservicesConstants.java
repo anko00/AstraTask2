7f98fb21e88cd4b9dfb69e9d685991958b278c3c
Anko00 <finn.mcmisolt@gmail.com>
2022-11-14 19:02:34 +0700

    init

---
+++ bin/custom/astrawebservices/gensrc/org/astra/training/webservices/constants/GeneratedYcommercewebservicesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 14 Nov 2022 17.10.25                        ---
+ * ----------------------------------------------------------------
+ */
+package org.astra.training.webservices.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYcommercewebservicesConstants
+{
+	public static final String EXTENSIONNAME = "astrawebservices";
+	
+	protected GeneratedYcommercewebservicesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
